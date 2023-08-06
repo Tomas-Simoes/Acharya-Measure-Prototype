9589566d2fdb0176282d2d644300067ac3d33833
Tomas-Simoes <tomas20simoes@gmail.com>
2023-08-06 02:19:11 +0100

    Added saving images after prediction

--- Utlis.py
+++ Utlis.py
@@ -1,5 +1,9 @@
 import cv2
 import os
+import numpy as np
+from PIL import Image
+
+cachedPredictionPath = ""
 
 def drawRectangle(img, label, x1, y1, x2, y2):
   boundingBox = cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
@@ -22,6 +26,43 @@ def readImage(path):
   _img = cv2.imread(path)
   _img = cv2.resize(_img, (960, 540))
   return _img
+
+def saveImage(image, path, fileName):
+  try:
+    global cachedPredictionPath
+
+    rootPath = os.path.dirname(os.path.abspath(__file__))
+    outputPath = os.path.join(rootPath, path)
+
+    predictionsFolders = [item for item in os.listdir(outputPath) if os.path.isdir(os.path.join(outputPath, item))]
+    
+    if not cachedPredictionPath:
+      for predictionFolder in predictionsFolders:
+        lastPredictionFolderNumber = ''.join(filter(str.isdigit, predictionFolder[-2:]))
+      
+      if len(predictionsFolders) == 0:
+          lastPredictionFolderNumber = "0"
+
+      outputPath = os.path.join(outputPath, f'Prediction {int(lastPredictionFolderNumber) + 1}')
+      cachedPredictionPath = outputPath
+
+      print(outputPath)
+      print(lastPredictionFolderNumber)
+    
+
+    os.makedirs(cachedPredictionPath, exist_ok=True)
+    
+    outputPath = os.path.join(cachedPredictionPath, fileName)
+
+    saved = cv2.imwrite(outputPath, image)
+    
+    if saved : print(f'The image was sucessfully saved in path "{outputPath}"')
+    else: print(f'The image was not sucessfully saved in path "{outputPath}"')
+
+  except Exception as e:
+    print(f'Error occurred while saving the image: {e}')
+
+
 
 def readPath(path):
   _allImagesInPath = []
