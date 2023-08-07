daf3c34ac02481600397f77679c32cbd4aef48e9
Tomas-Simoes <tomas20simoes@gmail.com>
2023-08-07 18:36:07 +0100

    Added distance prediction

--- AcharyaMeasuringPrototype.py
+++ AcharyaMeasuringPrototype.py
@@ -1,59 +1,98 @@
 import cv2
-import glob 
+import glob
 import Utlis as utlis
 from ultralytics import YOLO
 from PIL import Image
 
-#? Path to use for predictions
-imagePath = "Test Images/"
+# ? Path to use for predictions
+testImagePath = "Test Images/"
+testImageDataPath = "Test Images/measurements.txt"
 
-#? Path to use for saving the predictions
+# ? Path to use for saving the predictions
 savingPath = "Predicted Images"
 
-#? Model to use
+# ? Model to use
 modelName = "Models/best.pt"
 
-#? Configs
+# ? Configs
 runTraining = False
-saveImagesAfterPrediction = True
-useReferencePoint = True;
+saveImagesAfterPrediction = False
 epochsNumber = 10
 
-#? Global Variables
+# ? Camera Settings
+focalLength = 25
+
+sensorHeight = 14.9
+sensorWidth = 22.3
+
+imageWidth = 960
+imageHeight = 560
+
+# ? Global Variables
 allImages = []
+testImagesData = []
+
 
 def init():
-  allImages = utlis.readPath(imagePath)
-   
-  imageNumber = 0
+    global testImagesData
+    global allImages
 
-  for image in allImages:
-    imageNumber = imageNumber + 1
+    allImages = utlis.readPath(testImagePath, imageWidth, imageHeight)
 
-    recognizeObjects(image)
+    with open(testImageDataPath, "r") as configFile:
+        testImagesData = configFile.readlines()
 
-    if saveImagesAfterPrediction: utlis.saveImage(image, f'{savingPath}', f"PredictedImage_{imageNumber}.jpg")
+    imageNumber = 0
 
-    cv2.imshow(f'Image {imageNumber}', image)
-    
-  
-  cv2.waitKey(0)
+    for image in allImages:
+        imageNumber = imageNumber + 1
+
+        recognizeObjects(image, imageNumber)
+
+        if saveImagesAfterPrediction:
+            utlis.saveImage(image, f'{savingPath}',
+                            f"PredictedImage_{imageNumber}.jpg")
+
+        cv2.imshow(f'Image {imageNumber}', image)
+
+    cv2.waitKey(0)
 
 
-def recognizeObjects(img):
-  model = YOLO(modelName)
+def recognizeObjects(image, imageNumber):
+    model = YOLO(modelName)
 
-  if runTraining: model.train(data="data.yaml", epochs=epochsNumber)
-  
-  result = model.predict(img)[0]
+    if runTraining:
+        model.train(data="data.yaml", epochs=epochsNumber)
 
-  for box in result.boxes:
-    label, x1, x2, y1, y2 = utlis.getObjectInformation(box, result)
-    utlis.drawRectangle(img, label, x1, x2, y1, y2)
-    
+    result = model.predict(image)[0]
+
+    for box in result.boxes:
+        className, conf, x1, x2, y1, y2 = utlis.getObjectInformation(
+            box, result)
+
+        windowDistance = ""
+        if (className == "window"):
+            windowDistance = recognizeWindowDistance(imageNumber, y1, y2)
+
+        utlis.drawRectangle(image, className, conf, x1,
+                            x2, y1, y2, windowDistance)
+
+
+def recognizeWindowDistance(imageNumber, y1, y2):
+    _, windowWidth, windowHeight = utlis.getWindowInformation(
+        imageNumber, testImagesData)
+
+    windowDistance = (focalLength * (float(windowHeight) * 10)
+                      * imageHeight) / ((y1 - y2) * sensorHeight)
+    print(
+        f'Calculating formula with height: {focalLength} * {float(windowHeight) * 10} * {imageHeight} / {y1 - y2} * {sensorHeight} = {windowDistance}')
+
+    windowDistance = round(windowDistance) / 10
+
+    return windowDistance
+
 
 init()
 
-#recognizedObjects = recognizeObjects(image)
-#recognizedObjects.show()
-
+# recognizedObjects = recognizeObjects(image)
+# recognizedObjects.show()
--- Utlis.py
+++ Utlis.py
@@ -2,76 +2,127 @@ import cv2
 import os
 import numpy as np
 from PIL import Image
+# from AcharyaMeasuringPrototype import imageHeight
+# from AcharyaMeasuringPrototype import imageWidth
 
 cachedPredictionPath = ""
 
-def drawRectangle(img, label, x1, y1, x2, y2):
-  boundingBox = cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
-  (text_w, text_h), _ = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.6, 1)
-  boundingBox = cv2.rectangle(boundingBox, (x1, y1 - 20), (x1 + text_w, y1), (255, 0, 0), -1)
-  boundingBox = cv2.putText(boundingBox, label, (x1, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1)
+def drawRectangle(img, className, conf, x1, y1, x2, y2, windowDistance):
+    label = className + " " + str(conf) + "%"
+
+    boundingBox = cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+    (text_w, text_h), _ = cv2.getTextSize(
+        label, cv2.FONT_HERSHEY_SIMPLEX, 0.6, 1)
+    boundingBox = cv2.rectangle(
+        boundingBox, (x1, y1 - 20), (x1 + text_w, y1), (255, 0, 0), -1)
+    boundingBox = cv2.putText(boundingBox, label, (x1, y1 - 5),
+                              cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1)
+    if className == "window":
+        boundingBox = cv2.putText(
+            boundingBox, f'Window Distance: {str(windowDistance)} cm', (x1, y1 - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1)
+
 
 def getObjectInformation(box, result):
-  class_name = result.names[box.cls[0].item()]
-  cords = box.xyxy[0].tolist()
-  cords = [round(x) for x in cords]
-  x1, y1, x2, y2 = cords
-  conf = round(box.conf[0].item(), 2)
+    class_name = result.names[box.cls[0].item()]
+    cords = box.xyxy[0].tolist()
+    cords = [round(x) for x in cords]
+    x1, y1, x2, y2 = cords
+    conf = round(box.conf[0].item(), 2)
 
-  boundingBox_label = class_name + " " + str(conf) + "%"
-  return boundingBox_label, x1, y1, x2, y2
+    return class_name, conf, x1, y1, x2, y2
 
-def readImage(path):
-  _img = cv2.imread(path)
-  _img = cv2.resize(_img, (960, 540))
-  return _img
+
+def getWindowInformation(imageNumber, data):
+    print(
+        f'Reading distance between camera and window for image number {imageNumber}')
+
+    thisWindowData = ""
+
+    for imageData in data:
+        if imageData[0] == str(imageNumber):
+            thisWindowData = imageData.rstrip('\n')
+
+    if thisWindowData != "":
+        print(f'Found windows data for this image "{thisWindowData}".')
+    else:
+        print(
+            "Didn't found windows data for this image. Skiping window distance recognition.")
+        return 0, 0, 0
+
+    newInformation = ""
+    allExtractedInformation = []
+
+    for character in thisWindowData:
+        readCharacters = character != " "
+
+        if (readCharacters):
+            newInformation = newInformation + character
+        else:
+            allExtractedInformation.append(newInformation)
+            newInformation = ""
+
+    allExtractedInformation.append(newInformation)
+    return allExtractedInformation
+
 
 def saveImage(image, path, fileName):
-  try:
-    global cachedPredictionPath
+    try:
+        global cachedPredictionPath
 
-    rootPath = os.path.dirname(os.path.abspath(__file__))
-    outputPath = os.path.join(rootPath, path)
+        rootPath = os.path.dirname(os.path.abspath(__file__))
+        outputPath = os.path.join(rootPath, path)
 
-    predictionsFolders = [item for item in os.listdir(outputPath) if os.path.isdir(os.path.join(outputPath, item))]
-    
-    if not cachedPredictionPath:
-      for predictionFolder in predictionsFolders:
-        lastPredictionFolderNumber = ''.join(filter(str.isdigit, predictionFolder[-2:]))
-      
-      if len(predictionsFolders) == 0:
-          lastPredictionFolderNumber = "0"
+        predictionsFolders = [item for item in os.listdir(
+            outputPath) if os.path.isdir(os.path.join(outputPath, item))]
 
-      outputPath = os.path.join(outputPath, f'Prediction {int(lastPredictionFolderNumber) + 1}')
-      cachedPredictionPath = outputPath
+        if not cachedPredictionPath:
+            for predictionFolder in predictionsFolders:
+                lastPredictionFolderNumber = ''.join(
+                    filter(str.isdigit, predictionFolder[-2:]))
+
+            if len(predictionsFolders) == 0:
+                lastPredictionFolderNumber = "0"
+
+            outputPath = os.path.join(
+                outputPath, f'Prediction {int(lastPredictionFolderNumber) + 1}')
+            cachedPredictionPath = outputPath
+
+        os.makedirs(cachedPredictionPath, exist_ok=True)
+
+        outputPath = os.path.join(cachedPredictionPath, fileName)
+
+        saved = cv2.imwrite(outputPath, image)
+
+        if saved:
+            print(f'The image was sucessfully saved in path "{outputPath}"')
+        else:
+            print(
+                f'The image was not sucessfully saved in path "{outputPath}"')
+
+    except Exception as e:
+        print(f'Error occurred while saving the image: {e}')
 
 
-    os.makedirs(cachedPredictionPath, exist_ok=True)
-    
-    outputPath = os.path.join(cachedPredictionPath, fileName)
+def readPath(path, imageNewWidth, imageNewHeight):
+    _allImagesInPath = []
+    _imagesExtensions = [".jpg", ".jpeg", ".png"]
 
-    saved = cv2.imwrite(outputPath, image)
-    
-    if saved : print(f'The image was sucessfully saved in path "{outputPath}"')
-    else: print(f'The image was not sucessfully saved in path "{outputPath}"')
+    for _fileName in os.listdir(path):
+        if any(_fileName.endswith(extension) or _fileName.endswith(extension.upper()) for extension in _imagesExtensions):
+            _thisImagePath = os.path.join(path, _fileName)
 
-  except Exception as e:
-    print(f'Error occurred while saving the image: {e}')
+            _thisImage = cv2.imread(_thisImagePath)
+            _thisImage = cv2.resize(
+                _thisImage, (imageNewWidth, imageNewHeight))
+
+            _allImagesInPath.append(_thisImage)
+
+    return _allImagesInPath
 
 
-
-def readPath(path):
-  _allImagesInPath = []
-  _imagesExtensions = [".JPG", ".jpeg", ".png", ""]
-
-  for _fileName in os.listdir(path):
-    if any(_fileName.endswith(extension) for extension in _imagesExtensions):  
-      _thisImagePath = os.path.join(path, _fileName)
-
-      _thisImage = cv2.imread(_thisImagePath)
-      _thisImage = cv2.resize(_thisImage, (960, 540))
-
-      _allImagesInPath.append(_thisImage)
-
-  return _allImagesInPath
+def readImage(path, imageNewWidth, imageNewHeight):
+    _img = cv2.imread(path)
+    _img = cv2.resize(_img, (imageNewWidth, imageNewHeight))
+    return _img
