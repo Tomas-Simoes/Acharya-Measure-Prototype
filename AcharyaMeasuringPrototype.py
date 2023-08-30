36806f7c11d10e8b5d43d0290fd2f28ad0a890da
Tomas-Simoes <tomas20simoes@gmail.com>
2023-08-30 01:35:57 +0100

    Added proportion function and Lamnda files

--- AcharyaMeasuringPrototype.py
+++ AcharyaMeasuringPrototype.py
@@ -1,9 +1,9 @@
 import os
 import cv2
-import math
 import numpy as np
-import Utlis as utlis
 from ultralytics import YOLO
+
+import Utlis as utlis
 import ImageProcessing as imageProcessing
 
 # ? Path to use for predictions
@@ -14,18 +14,21 @@ testImageDataPath = "Test Images/measurements.txt"
 savingPath = "Predicted Images"
 
 # ? Model to use
-# modelName = "Models/best.pt"
+modelName = "Models/best.pt"
 # modelName = "Models/bestWallWindow.pt"
-modelName = "yolov8m.pt"
+# modelName = "yolov8m.pt"
 model = None
 
 # ? Configs
-runTraining = True
-runPrediction = False
+runTraining = False
+runPrediction = True
 findVanishingPoints = False
 saveImagesAfterPrediction = False
 resizeImage = False
+chooseWallBounderies = True
+
 epochsNumber = 30
+minimumWindowConfRate = 0.6
 
 # ? Camera Settings
 focalLength = 28
@@ -40,6 +43,9 @@ imageHeight = 560
 # ? Global Variables
 allImages = []
 testImagesData = []
+cachedMousePositionX = -1
+cachedMousePositionY = -1
+recognitionResults = None
 
 
 def init():
@@ -47,7 +53,7 @@ def init():
 
 
 def startPrototype():
-    global testImagesData, allImages, imageWidth, imageHeight, model
+    global testImagesData, allImages, imageWidth, imageHeight, model, recognitionResults
 
     model = YOLO(modelName)
 
@@ -64,6 +70,7 @@ def startPrototype():
 
     for image in allImages:
         imageNumber = imageNumber + 1
+        imageName = f'Image {imageNumber}'
 
         if not resizeImage:
             imageHeight, imageWidth, _ = image.shape
@@ -72,13 +79,18 @@ def startPrototype():
             recognizeVanishingPoints(image, imageNumber)
 
         if runPrediction:
-            recognizeObjects(image, imageNumber)
+            recognitionResults = recognizeObjects(
+                image, imageNumber)
 
         if saveImagesAfterPrediction:
             utlis.saveImage(image, f'{savingPath}',
                             f"PredictedImage_{imageNumber}.jpg")
 
-        cv2.imshow(f'Image {imageNumber}', image)
+        cv2.imshow(imageName, image)
+
+        if (chooseWallBounderies):
+            cv2.setMouseCallback(
+                imageName, chooseWallBounderies, (image, imageName, recognitionResults))
 
     cv2.waitKey(0)
 
@@ -91,17 +103,20 @@ def recognizeObjects(image, imageNumber):
         className, conf, x1, y1, x2, y2 = utlis.getObjectInformation(
             box, result)
 
-        if (conf <= 0.5):
+        if (conf <= minimumWindowConfRate):
             print(
                 f'Recognized one {className} with less than 50% confidence rate.')
             continue
 
         windowDistance = ""
+
         if (className == "window"):
             windowDistance = recognizeWindowDistance(imageNumber, y1, y2)
 
-        utlis.drawRectangle(image, className, conf, x1,
-                            y1, x2, y2, windowDistance)
+        utlis.drawClassRectangle(image, className, conf, x1,
+                                 y1, x2, y2, windowDistance)
+
+    return result.boxes
 
 
 def recognizeWindowDistance(imageNumber, y1, y2):
@@ -146,6 +161,77 @@ def recognizeVanishingPoints(image, imageNumber):
         i += 1
 
     # cv2.imshow(f'Pre-processed Image {imageNumber}', preProcessedImage)
+
+
+def chooseWallBounderies(event, x, y, flags, param):
+    global cachedMousePositionX, cachedMousePositionY
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        (image, imageName, recognitionResults) = param
+
+        cv2.circle(image, (x, y), 5, (0, 0, 255), -1)
+        cv2.imshow(imageName, image)
+
+        if cachedMousePositionX == -1 or cachedMousePositionY == -1:
+            print("First point for the wall choosen. Choose another one.")
+            cachedMousePositionX, cachedMousePositionY = x, y
+        else:
+            print("Two points choosen.")
+
+            point1 = (cachedMousePositionX, cachedMousePositionY)
+            point2 = (x, y)
+
+            cv2.line(image, point1, point2, (0, 0, 255), 5)
+            cv2.imshow(imageName, image)
+
+            for box in recognitionResults:
+                conf = round(box.conf[0].item(), 2)
+
+                if (conf >= minimumWindowConfRate):
+                    cords = box.xyxy[0].tolist()
+                    cords = [round(x) for x in cords]
+                    x1, y1, x2, y2 = cords
+
+                    print(
+                        f'Choosing window with coordinates (({x1}, {y1}), ({x2, y2}))')
+
+                    if y1 > y2:
+                        windowHeightPixels = y1 - y2
+                    else:
+                        windowHeightPixels = y2 - y1
+
+                    _, _, windowHeightCM = utlis.getWindowInformation(
+                        imageName.split()[-1], testImagesData)
+
+                    recognizeWallHeight(
+                        image, imageName, windowHeightPixels, windowHeightCM, point1, point2)
+
+                    return
+
+            print(
+                "There is no available windows in the image. Skipping wall height recognition.")
+
+
+def recognizeWallHeight(image, imageName, windowHeightPixels, windowHeightCM, point1, point2):
+    x1, y1 = point1
+    x2, y2 = point2
+
+    if y1 > y2:
+        wallHeightPixels = y1 - y2
+    else:
+        wallHeightPixels = y2 - y1
+
+    print(f'Window height in CM: {windowHeightCM}')
+    print(f'Window height in Pixels: {windowHeightPixels}')
+    print(f'Wall height in Pixels: {wallHeightPixels}')
+
+    wallHeightCM = round((int(windowHeightCM) * wallHeightPixels) /
+                         int(windowHeightPixels))
+
+    # ? Draw information rectangle
+    print(f'Wall height in CM: {wallHeightCM}')
+
+    cv2.imshow(imageName, image)
 
 
 def trainModel(model):
